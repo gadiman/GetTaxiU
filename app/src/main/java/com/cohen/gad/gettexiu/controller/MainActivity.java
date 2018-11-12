24b04ff5c0783a2e178270de0b663242379d2709
Gad Cohen <gadiman@gmail.com>
2018-11-12 12:19:29 +0200

    start whit the layot file

--- app/src/main/java/com/cohen/gad/gettexiu/controller/MainActivity.java
+++ app/src/main/java/com/cohen/gad/gettexiu/controller/MainActivity.java
@@ -2,8 +2,11 @@ package com.cohen.gad.gettexiu.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.cohen.gad.gettexiu.R;
+import com.cohen.gad.gettexiu.model.entities.Travel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        mySpinner.setAdapter(new ArrayAdapter<Travel.TRAVEL_STATUS>(this, android.R.layout.simple_spinner_item, Travel.TRAVEL_STATUS.values()));
 
     }
 }
