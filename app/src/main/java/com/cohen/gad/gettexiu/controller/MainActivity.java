9ec751dd92a3014e6b60ac51993ea091da80afd9
Gad Cohen <gadiman@gmail.com>
2018-11-18 09:02:08 +0200

    style both, Main screen and the second screen add animation submit button fix place picker

--- app/src/main/java/com/cohen/gad/gettexiu/controller/MainActivity.java
+++ app/src/main/java/com/cohen/gad/gettexiu/controller/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-
+//f
     private EditText NameTextInput;
     private EditText EmailTextInput;
     private EditText InitialLocationTextInput;
