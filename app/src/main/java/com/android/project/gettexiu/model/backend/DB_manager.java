b01e7fe78e073b858f7cbdf7bbb1248cbb0b866c
Gad Cohen <gadiman@gmail.com>
2018-11-29 10:55:18 +0200

    fix the firebase

--- app/src/main/java/com/android/project/gettexiu/model/backend/DB_manager.java
+++ app/src/main/java/com/android/project/gettexiu/model/backend/DB_manager.java
@@ -5,15 +5,9 @@ import android.content.ContentValues;
 public interface DB_manager {
 
     //------------------------------- Functions -------------------------------//
-    public void addNewTravel(ContentValues travel);
+    public String addNewTravel(ContentValues travel);
 
     public boolean checkIfTravelAdded(String id);
 
-    public interface Action<T> {
-        void onSuccess(T obj);
 
-        void onFailure(Exception exception);
-
-        void onProgress(String status, double percent);
-    }
 }
--- app/src/main/java/com/android/project/gettexiu/model/datasource/FireBase_DBTravel.java
+++ app/src/main/java/com/android/project/gettexiu/model/datasource/FireBase_DBTravel.java
@@ -1,27 +1,29 @@
 package com.android.project.gettexiu.model.datasource;
 
 import android.content.ContentValues;
-import android.os.AsyncTask;
 
 import com.android.project.gettexiu.model.backend.DB_manager;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import static com.android.project.gettexiu.model.backend.TravelConst.TravelC.CUSTOMER_PHONE;
 
 
 public class FireBase_DBTravel  implements DB_manager {
     @Override
-    public void addNewTravel(ContentValues travel) {
+    public String addNewTravel(ContentValues travel) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference firebaseRef = database.getReference( "gettexiu-ea883" );
-        firebaseRef.child( travel.getAsString( CUSTOMER_PHONE ) ).setValue( travel );
+        DatabaseReference firebaseRef = database.getReference( "Travels" );
+
+        //create a new key for this travel
+        DatabaseReference TravelRef = firebaseRef.push();
+        //Insert the object to the FireBase
+        TravelRef.setValue(travel);
+        //return the key for checking if the insert succeeded
+        return TravelRef.push().getKey();
     }
 
     @Override
     public boolean checkIfTravelAdded(String id) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference firebaseRef = database.getReference( "gettexiu-ea883" );
-        return false;
+        return id == FirebaseDatabase.getInstance().getReference("Travels").child(id).getKey();
     }
 
 
